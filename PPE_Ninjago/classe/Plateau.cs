f5c2d8030d25939ff4e81697690eb935b12b3962
Axel <Axel@LAPTOP-IGLJCD8M>
2017-10-13 08:52:17 +0200

    plateau + case

---
+++ PPE_Ninjago/classe/Plateau.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPE_Ninjago.classe
+{
+    class Plateau
+    {
+        int idPlateau;
+        int formatP;
+
+        public Plateau(int idPlateau, int formatP)
+        {
+            this.idPlateau = idPlateau;
+            this.formatP = formatP;
+        }
+
+        public int IdPlateau
+        {
+            get
+            {
+                return idPlateau;
+            }
+
+            set
+            {
+                idPlateau = value;
+            }
+        }
+
+        public int FormatP
+        {
+            get
+            {
+                return formatP;
+            }
+
+            set
+            {
+                formatP = value;
+            }
+        }
+
+       
+    }
+}
--- PPE_Ninjago/classe/Program.cs
+++ PPE_Ninjago/classe/Program.cs
@@ -17,6 +17,8 @@ namespace PPE_Ninjago
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+
         }
     }
 }
