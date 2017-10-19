17696dfcf064967dece8fb6e3ebc1da66990875c
Axel <Axel@LAPTOP-IGLJCD8M>
2017-10-19 14:45:46 +0200

    Revert "Pour Axel"
    This reverts commit 96012babce29183390ca36f635df5ff87763c167.

---
+++ PPE_Ninjago/classe/collection.cs
@@ -0,0 +1,31 @@
+﻿using PPE3_Ninjago;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPE_Ninjago.classe
+{
+    class collection
+    {
+        string Nom;
+        DateTime datecreation;
+        List<Carte> colleCartes;
+
+        public void recupTest()
+        {
+            /*adfhjvyjuhfvkj*/
+        }
+
+        public collection(string nom, DateTime datecreation, List<Carte> collection)
+        {
+            Nom = nom;
+            this.datecreation = datecreation;
+            this.colleCartes = new List<Carte>();
+        }
+
+
+       
+    }
+}
